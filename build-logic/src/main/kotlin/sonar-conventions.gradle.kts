d62c36496584d6acbd240931d98a5459a8171260
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2024-01-24 22:35:53 +0100

    chore(build): upgrade Gradle to 8.5.0

--- build-logic/src/main/kotlin/sonar-conventions.gradle.kts
+++ build-logic/src/main/kotlin/sonar-conventions.gradle.kts
@@ -6,7 +6,7 @@ sonar {
     properties {
         property("sonar.sourceEncoding", "UTF-8")
         property("sonar.sources", "")  // nothing in the root
-        property("sonar.dependencyCheck.jsonReportPath", "${buildDir}/reports/dependency-check-report.json")
-        property("sonar.dependencyCheck.htmlReportPath", "${buildDir}/reports/dependency-check-report.html")
+        property("sonar.dependencyCheck.jsonReportPath", rootProject.layout.buildDirectory.file("reports/dependency-check-report.json"))
+        property("sonar.dependencyCheck.htmlReportPath", rootProject.layout.buildDirectory.file("reports/dependency-check-report.html"))
     }
 }
